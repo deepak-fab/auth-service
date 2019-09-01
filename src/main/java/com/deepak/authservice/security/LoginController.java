6f513221006733ee5ec62335c4914f8a580e91d2
Deepak Mittal <deepak.mittal@ad.infosys.com>
2019-09-01 21:26:36 +0530

    auth service

--- src/main/java/com/deepak/authservice/security/LoginController.java
+++ src/main/java/com/deepak/authservice/security/LoginController.java
@@ -3,6 +3,7 @@ package com.deepak.authservice.security;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,4 +38,12 @@ public class LoginController {
 		 return new UserInfo(); 
 		
 	}
+	
+	@GetMapping("/check")
+	public String checkStatus() {
+		return "Service is up";
+	}
+	
+	
+	
 }
